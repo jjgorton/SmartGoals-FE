80207258ca9cf58ec1547e29722ac28f771f12a4
jjgorton <jjgorton@gmail.com>
2019-12-28 05:38:55 -0500

    added Router

--- smart-goals/src/App.js
+++ smart-goals/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+
+import Routes from './components/Routes';
 
 function App() {
     return (
-        <div className='App'>
+        <Router className='App'>
             <h1>Smart Goals</h1>
-        </div>
+            <Routes />
+        </Router>
     );
 }
 
---
+++ smart-goals/src/components/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+const Routes = props => {
+    return (
+        <>
+            <h2>Routes</h2>
+        </>
+    );
+};
+
+export default Routes;
