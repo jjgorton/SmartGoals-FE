2a0fd51f49e695f327ed06c98e0d8b632131828c
jjgorton <jjgorton@gmail.com>
2020-04-09 20:40:59 -0400

    basic styling for welcome screen

--- smart-goals/src/components/welcome/Welcome.js
+++ smart-goals/src/components/welcome/Welcome.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './welcome.scss';
 
 const Welcome = () => {
     return (
-        <div>
-            <h2>Welcome to the Smart Goals app!</h2>
-            <p>
+        <div className='welcome'>
+            <h2>Welcome to Smart Goals!</h2>
+            <p className='instructions'>
                 <Link to='/login'>Login</Link> and create a new workspace to
                 start achieving your goals, or join your team's workspace to
                 plan and accomplish together.
             </p>
-            <p>
+            <p className='instructions'>
                 Don't have an account yet? <Link to='/register'>Register</Link>{' '}
                 to create a new one.
             </p>
