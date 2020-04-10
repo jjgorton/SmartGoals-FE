1aad892d1664caccaaa8705b21f9ec9780b4355b
jjgorton <jjgorton@gmail.com>
2020-04-09 21:58:16 -0400

    built register component

--- smart-goals/src/components/auth/Register.js
+++ smart-goals/src/components/auth/Register.js
@@ -1,9 +1,88 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const Register = () => {
+import { register } from '../../actions/authActions';
+
+import './auth.scss';
+
+const Register = (props) => {
+    const state = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: '',
+        email: '',
+    });
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleChanges = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        localStorage.clear();
+        dispatch(register(credentials)).then(() => {
+            props.history.push('/user/workspaces');
+        });
+    };
+
+    useEffect(() => {
+        if (state.error && state.error.response) {
+            setErrorMessage(state.error.response.data.message);
+        } else if (state.error) {
+            setErrorMessage(state.error.message);
+        }
+    }, [state.error]);
+
     return (
-        <div>
-            <h1>Register form here</h1>
+        <div className='auth-container'>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div className='input'>
+                    <label htmlFor='username'>Username:</label>
+                    <input
+                        type='text'
+                        id='username'
+                        name='username'
+                        placeholder='Username'
+                        autoComplete='username'
+                        value={credentials.username}
+                        onChange={handleChanges}
+                        required
+                    />
+                </div>
+                <div className='input'>
+                    <label htmlFor='password'>Password:</label>
+                    <input
+                        type='password'
+                        id='password'
+                        name='password'
+                        placeholder='Password'
+                        autoComplete='current-password'
+                        value={credentials.password}
+                        onChange={handleChanges}
+                        required
+                    />
+                </div>
+                <div className='input'>
+                    <label htmlFor='email'>Email*:</label>
+                    <input
+                        type='email'
+                        id='email'
+                        name='email'
+                        placeholder='Email - Optional'
+                        value={credentials.email}
+                        onChange={handleChanges}
+                    />
+                </div>
+                <button>Login</button>
+
+                {errorMessage && <p>{errorMessage}</p>}
+            </form>
         </div>
     );
 };
