2da3db2eb7c0229b06ada46194b8974689b81be3
jjgorton <jjgorton@gmail.com>
2021-04-02 17:24:34 -0400

    normalize rank completed with tests

--- smart-goals/src/utils/normalizeRank/index.js
+++ smart-goals/src/utils/normalizeRank/index.js
@@ -6,8 +6,15 @@ export default (objList, indexOfNewObj) => {
     const next =
         indexOfNewObj < objList.length - 1 ? objList[indexOfNewObj + 1] : null;
 
+    // simply adding a new obj to end will add 100 from last rank
+    if (!next && prev) {
+        newObj.rank = prev.rank + 100;
+        return [newObj];
+    }
+
     const diff = (next && next.rank) - (prev && prev.rank);
-    if (diff < 2) {
+
+    if (diff < 3) {
         //assign new ranks for all
         return objList.map((obj, i) => {
             obj.rank = (i + 1) * 100;
@@ -16,7 +23,7 @@ export default (objList, indexOfNewObj) => {
     }
 
     //else return [new obj] with rank set to halfway between prev and next
-    newObj.rank = next.rank - diff / 2;
+    newObj.rank = next.rank - Math.floor(diff / 2);
 
     return [newObj];
 };
