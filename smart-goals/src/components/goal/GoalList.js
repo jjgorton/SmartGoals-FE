320f3ed47d3d74020f0ee4f77f54f6e75dfe86bd
jjgorton <jjgorton@gmail.com>
2021-04-02 20:49:14 -0400

    added react-beautiful-dnd to goals

--- smart-goals/src/components/goal/GoalList.js
+++ smart-goals/src/components/goal/GoalList.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import Goal from './Goal';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 import './goalList.scss';
 
@@ -15,11 +16,21 @@ const GoalList = () => {
     //     return <Loader type='Triangle' color='blue' height={50} width={50} />;
     // }
     return (
-        <div className='goal-list'>
-            {goals.list.map((goal, i) => (
-                <Goal key={i} goal={goal} />
-            ))}
-        </div>
+        <DragDropContext>
+            <Droppable droppableId='goals'>
+                {(provided) => (
+                    <div
+                        className='goal-list'
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}>
+                        {goals.list.map((goal, i) => (
+                            <Goal key={i} goal={goal} index={i} />
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
     );
 };
 
